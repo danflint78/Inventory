fdb68c835eeb223f64fba6084805274086a0ca58
danflint78 <danflint78@gmail.com>
2015-07-24 22:07:49 -0400

    jQuery seperation and update.

--- InventorySite/App_Start/BundleConfig.cs
+++ InventorySite/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace InventorySite
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/slideshow").Include(
+                     "~/Scripts/slideshow.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
